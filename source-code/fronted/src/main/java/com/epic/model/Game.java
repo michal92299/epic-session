859f97d61b9f5e86bc6945c51577b4918de70121
michal92299 <chudybyczek2137@gmail.com>
2025-11-15 22:17:59 +0100

    Create Game.java

---
+++ source-code/fronted/src/main/java/com/epic/model/Game.java
@@ -0,0 +1,22 @@
+package com.epic.model;
+
+public class Game {
+    private String appName;
+    private String title;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
